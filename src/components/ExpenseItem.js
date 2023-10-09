cedf4c55069531a870e554495f5e1713be5b6bfd
big-peanut <sandeepsundarlenka22@gmail.com>
2023-10-09 08:26:06 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>DATE</div>
+      <div>
+        <h2>TITLE</h2>
+      </div>
+      <div>AMOUNT</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
