fdd823e8884b02f74f89466676fb97b473c8b8d7
big-peanut <sandeepsundarlenka22@gmail.com>
2023-10-10 08:28:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "car insurance", amount: 299, date: new Date(2023, 2, 28) },
+    { title: "laptop repair", amount: 199, date: new Date(2023, 2, 27) },
+    { title: "long drive", amount: 999, date: new Date(2023, 2, 26) },
+    { title: "internet bill", amount: 499, date: new Date(2023, 2, 25) },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const expenseDate = props.date;
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
   return (
-    <div>
-      <div>DATE</div>
-      <div>
-        <h2>TITLE</h2>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
-      <div>AMOUNT</div>
     </div>
   );
 }
